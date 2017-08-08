b6e0315c2602cd8cb1ac95e10b402a4b46a38bda
Kaido Iwamoto <h6824.kaido@gmail.com>
2017-08-08 09:48:00 +0900

    Support DATABASE_URL

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ Sequel::Model.plugin :timestamps, update_on_create: true
 Sequel.database_timezone = :utc
 Sequel.application_timezone = :local
 
-DB = Sequel.connect("sqlite://db/#{RACK_ENV}.sqlite3")
+DB = Sequel.connect(ENV["DATABASE_URL"] || "sqlite://db/#{RACK_ENV}.sqlite3")
 DB.loggers << Logger.new($stdout)
 
 class Post < Sequel::Model
